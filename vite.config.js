fa2d930729aaf79a9b838c33f3cd79ba36fa84d3
ruth <ruthsubexp@gmail.com>
2026-04-03 17:24:48 -0700

    proxy link changed in vite config and src file fofr index.html

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      // "/api": "http://localhost:5000",
+      "/api": "https://3jz9bj8avk.us-west-2.awsapprunner.com/api",
     },
   },
 });
